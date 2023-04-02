01957576e6dd265be60b1a1153fb39f93793d6df
Saurabh022mishra <skm80050@gmail.com>
2023-04-02 10:01:38 +0530

    IO Oprations

---
+++ IO.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+int main()
+{
+    char name[15] = "Farm House";
+    char size;
+    int personItServes;
+    float retailPrice;
+    printf("Enter the size of pizza as either 'S','M'or 'L'");
+    scanf("%c", &size);
+    printf("Enter the number of person it serves: ");
+    scanf("%d", &personItServes);
+    printf("Enter retail price:");
+    scanf("%f", &retailPrice);
+    printf("\nGenerating message...\n");
+    usleep(3000000);
+    printf("Yay! You ordered a %s Pizza of Size %c that serves %d.\nplease pay"
+           "Rs. %.1f to the delivery Person",
+           name, size, personItServes, retailPrice);
+
+    return 0;
+}
