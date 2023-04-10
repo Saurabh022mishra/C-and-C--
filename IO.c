0c80d1c1bc772de712a5cfe691cc35a15d416206
Saurabh022mishra <skm80050@gmail.com>
2023-04-10 23:24:17 +0530

    IO

--- IO.c
+++ IO.c
@@ -2,10 +2,12 @@
 #include <unistd.h>
 int main()
 {
-    char name[15] = "Farm House";
+    char name[20];
     char size;
     int personItServes;
     float retailPrice;
+    printf("Enter pizza name: ");
+    scanf("%[^\n]%*c",name);// read input until user press enter
     printf("Enter the size of pizza as either 'S','M'or 'L'");
     scanf("%c", &size);
     printf("Enter the number of person it serves: ");
@@ -13,10 +15,10 @@ int main()
     printf("Enter retail price:");
     scanf("%f", &retailPrice);
     printf("\nGenerating message...\n");
-    usleep(3000000);
+    //usleep(3000000);
     printf("Yay! You ordered a %s Pizza of Size %c that serves %d.\nplease pay"
            "Rs. %.1f to the delivery Person",
-           name, size, personItServes, retailPrice);
+           name, size, personItServes, retailPrice); 
 
     return 0;
 }
